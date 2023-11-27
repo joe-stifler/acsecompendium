25fb5243f2a0615ad9378e7bd049ed97fb35e991
acse-jrn22 <jose.neto23@imperial.ac.uk>
2023-11-27 19:37:43 +0000

    test: add simple test for acsecompendium.py

---
+++ tests/test_acsecompendium.py
@@ -0,0 +1,14 @@
+from acsecompendium import *
+from pytest import fixture
+
+# Use pytest fixtures to generate objects we know we'll reuse.
+# This makes sure tests run quickly
+
+@fixture(scope="module")
+def acsecompendium():
+    import acsecompendium
+    return acsecompendium
+
+
+def test_import(acsecompendium):
+    assert acsecompendium
